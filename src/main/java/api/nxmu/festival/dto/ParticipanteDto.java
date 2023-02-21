ddf31475e0cd7dd7a3a7fa359d75e0ff83a40abd
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2023-02-21 18:35:40 -0300

    criado, pacote dto e classe participanteDto

---
+++ src/main/java/api/nxmu/festival/dto/ParticipanteDto.java
@@ -0,0 +1,181 @@
+package api.nxmu.festival.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Builder
+@AllArgsConstructor
+@Getter
+@Setter
+public class ParticipanteDto {
+
+
+    private String nomeArtistico;
+    private String nomeResponsavel;
+    private String genero;
+    private String nascimento;
+    private String documentorg;
+    private String email;
+    private String necessidade;
+    private String descrinescessidade;
+    private String endereco;
+    private String cidade;
+    private String estado;
+    private String cep;
+    private String telefone;
+    private String musica;
+    private String gravacao;
+    private String tom;
+    private long opcao_categoria;
+    private long opcao_participante;
+    private byte[] fotoPerfil;
+
+    public ParticipanteDto(String nomeArtistico, String nomeResponsavel, String genero, String nascimento,
+            String documentorg, String email, String necessidade, String descrinescessidade, String endereco,
+            String cidade, String estado, String cep, String telefone, String musica, String gravacao, String tom,
+            long opcao_categoria, long opcao_participante) {
+        this.nomeArtistico = nomeArtistico;
+        this.nomeResponsavel = nomeResponsavel;
+        this.genero = genero;
+        this.nascimento = nascimento;
+        this.documentorg = documentorg;
+        this.email = email;
+        this.necessidade = necessidade;
+        this.descrinescessidade = descrinescessidade;
+        this.endereco = endereco;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.cep = cep;
+        this.telefone = telefone;
+        this.musica = musica;
+        this.gravacao = gravacao;
+        this.tom = tom;
+        this.opcao_categoria = opcao_categoria;
+        this.opcao_participante = opcao_participante;
+    }
+    public ParticipanteDto() {
+    }
+
+    public String getNomeArtistico() {
+        return nomeArtistico;
+    }
+    public void setNomeArtistico(String nomeArtistico) {
+        this.nomeArtistico = nomeArtistico;
+    }
+    public String getNomeResponsavel() {
+        return nomeResponsavel;
+    }
+    public void setNomeResponsavel(String nomeResponsavel) {
+        this.nomeResponsavel = nomeResponsavel;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    public String getNascimento() {
+        return nascimento;
+    }
+    public void setNascimento(String nascimento) {
+        this.nascimento = nascimento;
+    }
+    public String getDocumentorg() {
+        return documentorg;
+    }
+    public void setDocumentorg(String documentorg) {
+        this.documentorg = documentorg;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getNecessidade() {
+        return necessidade;
+    }
+    public void setNecessidade(String necessidade) {
+        this.necessidade = necessidade;
+    }
+    public String getDescrinescessidade() {
+        return descrinescessidade;
+    }
+    public void setDescrinescessidade(String descrinescessidade) {
+        this.descrinescessidade = descrinescessidade;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public String getCep() {
+        return cep;
+    }
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public String getMusica() {
+        return musica;
+    }
+    public void setMusica(String musica) {
+        this.musica = musica;
+    }
+    public String getGravacao() {
+        return gravacao;
+    }
+    public void setGravacao(String gravacao) {
+        this.gravacao = gravacao;
+    }
+    public String getTom() {
+        return tom;
+    }
+    public void setTom(String tom) {
+        this.tom = tom;
+    }
+    public long getOpcao_categoria() {
+        return opcao_categoria;
+    }
+    public void setOpcao_categoria(long opcao_categoria) {
+        this.opcao_categoria = opcao_categoria;
+    }
+    public long getOpcao_participante() {
+        return opcao_participante;
+    }
+    public void setOpcao_participante(long opcao_participante) {
+        this.opcao_participante = opcao_participante;
+    }
+    public byte[] getFotoPerfil() {
+        return fotoPerfil;
+    }
+    public void setFotoPerfil(byte[] fotoPerfil) {
+        this.fotoPerfil = fotoPerfil;
+    }
+}
