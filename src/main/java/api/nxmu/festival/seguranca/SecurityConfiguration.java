206383e77ed6f4f0a17ae40e1dd59fd43f997db6
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2023-04-24 13:19:42 -0300

    salva participacao autorizado rota publica

--- src/main/java/api/nxmu/festival/seguranca/SecurityConfiguration.java
+++ src/main/java/api/nxmu/festival/seguranca/SecurityConfiguration.java
@@ -27,7 +27,7 @@ public class SecurityConfiguration {
         .disable()
         .authorizeHttpRequests()
             .requestMatchers(HttpMethod.OPTIONS).permitAll()
-            .requestMatchers("/api/evento/auth/**", "/api/evento/salvaparticipante", "/api/apresentacoes").permitAll()
+            .requestMatchers("/api/evento/auth/**", "/api/evento/salvaparticipante", "/api/salvaapresentacao", "/api/apresentacoes").permitAll()
         .anyRequest()
         .authenticated()
         .and()
