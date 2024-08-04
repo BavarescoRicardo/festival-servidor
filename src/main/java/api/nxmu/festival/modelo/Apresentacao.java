1d218b861655e692f5ffebdfdc819778533c9799
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2024-08-03 22:54:36 -0300

    Ajustes para atribuir classificacoes
    foi trocado o mapeamento hibernate de apresentacoes na tabela classificacao, alem disso foi otimizado o metodo calculo de classificacoes

--- src/main/java/api/nxmu/festival/modelo/Apresentacao.java
+++ src/main/java/api/nxmu/festival/modelo/Apresentacao.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -22,8 +23,6 @@ import lombok.ToString;
 @Builder
 @AllArgsConstructor
 @Entity
-@Getter
-@Setter
 public class Apresentacao {
 
     @Id
@@ -51,7 +50,10 @@ public class Apresentacao {
     private List<Nota> notas; 
     
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "apresentacao")
-    private List<NotaFinal> notasfinais;        
+    private List<NotaFinal> notasfinais;    
+    
+    @OneToOne(mappedBy = "apresentacao")  // Corrected annotation
+    private Classificacao classificacao;    
 
     public Apresentacao(){
         
@@ -78,6 +80,128 @@ public class Apresentacao {
         this.autor = autor;
         this.individuos = individuos;
         this.categoria = categoria;
-    }    
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMusica() {
+		return musica;
+	}
+
+	public void setMusica(String musica) {
+		this.musica = musica;
+	}
+
+	public String getNomeartistico() {
+		return nomeartistico;
+	}
+
+	public void setNomeartistico(String nomeartistico) {
+		this.nomeartistico = nomeartistico;
+	}
+
+	public String getTom() {
+		return tom;
+	}
+
+	public void setTom(String tom) {
+		this.tom = tom;
+	}
+
+	public String getGravacao() {
+		return gravacao;
+	}
+
+	public void setGravacao(String gravacao) {
+		this.gravacao = gravacao;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getLinkmusica() {
+		return linkmusica;
+	}
+
+	public void setLinkmusica(String linkmusica) {
+		this.linkmusica = linkmusica;
+	}
+
+	public int getOrdem() {
+		return ordem;
+	}
+
+	public void setOrdem(int ordem) {
+		this.ordem = ordem;
+	}
+
+	public int getSenha() {
+		return senha;
+	}
+
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+
+	public int getIndividuos() {
+		return individuos;
+	}
+
+	public void setIndividuos(int individuos) {
+		this.individuos = individuos;
+	}
+
+	public int getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(int ativo) {
+		this.ativo = ativo;
+	}
+
+	public List<Participante> getParticipantes() {
+		return participantes;
+	}
+
+	public void setParticipantes(List<Participante> participantes) {
+		this.participantes = participantes;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public List<Nota> getNotas() {
+		return notas;
+	}
+
+	public void setNotas(List<Nota> notas) {
+		this.notas = notas;
+	}
+
+	public List<NotaFinal> getNotasfinais() {
+		return notasfinais;
+	}
+
+	public void setNotasfinais(List<NotaFinal> notasfinais) {
+		this.notasfinais = notasfinais;
+	}   
+    
+    
 
 }
